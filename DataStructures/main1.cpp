66ab52e1e4365574132495572144fd2f3813cf21
unknown <elayfr909@gmail.com>
2024-11-10 22:47:27 +0200

    Queue2

--- DataStructures/main1.cpp
+++ DataStructures/main1.cpp
@@ -1,29 +1,29 @@
-//#include "Queue.h"
-//
-//int main()
-//{
-//	Queue* Queue1 = new Queue;
-//	unsigned int num = 3;
-//	initQueue(Queue1, num);
-//
-//	std::cout << "Empty queue: " << isEmpty(Queue1) << std::endl;
-//
-//	enqueue(Queue1, 10);
-//	enqueue(Queue1, 20);
-//	enqueue(Queue1, 30);
-//
-//	std::cout << "index 0: " << Queue1->arr[0] << std::endl;
-//	std::cout << "index 1: " << Queue1->arr[1] << std::endl;
-//	std::cout << "index 2: " << Queue1->arr[2] << std::endl;
-//	std::cout << "Empty queue: " << isEmpty(Queue1) << std::endl;
-//	std::cout << "Full queue: " << isFull(Queue1) << std::endl;
-//
-//	dequeue(Queue1);
-//	std::cout << "Full queue: " << isFull(Queue1) << std::endl;
-//	std::cout << "index 0: " << Queue1->arr[0] << std::endl;
-//	std::cout << "index 1: " << Queue1->arr[1] << std::endl;
-//	std::cout << "index 2: " << Queue1->arr[2] << std::endl;
-//	cleanQueue(Queue1);
-//
-//	return 0;
-//}
+#include "Queue.h"
+
+int main()
+{
+	Queue* Queue1 = new Queue;
+	unsigned int num = 3;
+	initQueue(Queue1, num);
+
+	std::cout << "Empty queue: " << isEmpty(Queue1) << std::endl;
+
+	enqueue(Queue1, 10);
+	enqueue(Queue1, 20);
+	enqueue(Queue1, 30);
+
+	std::cout << "index 0: " << Queue1->arr[0] << std::endl;
+	std::cout << "index 1: " << Queue1->arr[1] << std::endl;
+	std::cout << "index 2: " << Queue1->arr[2] << std::endl;
+	std::cout << "Empty queue: " << isEmpty(Queue1) << std::endl;
+	std::cout << "Full queue: " << isFull(Queue1) << std::endl;
+
+	dequeue(Queue1);
+	std::cout << "Full queue: " << isFull(Queue1) << std::endl;
+	std::cout << "index 0: " << Queue1->arr[0] << std::endl;
+	std::cout << "index 1: " << Queue1->arr[1] << std::endl;
+	std::cout << "index 2: " << Queue1->arr[2] << std::endl;
+	cleanQueue(Queue1);
+
+	return 0;
+}
